f794cb6074bf2b58436b3e4bdc42baadb53226f1
Mysore Sridhar <118914178+sapphire2207@users.noreply.github.com>
2025-10-29 15:19:10 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://book-it-assignment.vercel.app/",
         changeOrigin: true,
         secure: false,
       },
